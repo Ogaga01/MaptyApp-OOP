b5ac8e1a3c99c7a483da2046c6ae35f4f4e8652d
Ogaga <iyaraogaga69@gmail.com>
2022-10-17 15:43:16 +0100

    create new workout using form

--- script.js
+++ script.js
@@ -10,33 +10,51 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+const workouts = []
 let map, mapEvent;
 
 class Workout {
-  constructor(type, distance, duration) {
-    this.type = type;
+    date = new Date()
+    id = (Date.now()+'').slice(-10)
+  constructor(coords, distance, duration) {
+    this.coords = coords;
     this.distance = distance;
     this.duration = duration;
   }
 }
 
 class Cycling extends Workout {
-  constructor(type, distance, duration, elevationGain) {
-    super(type, distance, duration);
-    this.elevationGain = elevationGain;
-  }
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+      this.elevationGain = elevationGain;
+      this.calcSpeed()
+    }
+    
+    calcSpeed() {
+        this.speed = this.distance / (this.duration / 60)
+        return this.speed
+    }
 }
 
 class Running extends Workout {
-  constructor(type, distance, duration, cadence) {
-    super(type, distance, duration);
-    this.cadence = cadence;
-  }
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+      this.cadence = cadence;
+      this.calcPace()
+    }
+    
+    calcPace() {
+        this.pace = this.duration / this.distance
+        return this.pace
+    }
 }
+
+const run = new Running([6, 5], 30, 600, 300)
+const ride = new Cycling([6, 5], 7, 11, 500)
+console.log(run, ride)
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position)
         const { latitude, longitude } = position.coords
         const coords = [latitude, longitude]
 
@@ -61,6 +79,59 @@ form.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log(mapEvent);
     const { lat, lng } = mapEvent.latlng;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const type = inputType.value;
+    let workout;
+    const validInputs = (...inputs) => {
+      return inputs.every(input => {
+        return Number.isFinite(input);
+      });
+    };
+    const positiveNumber = (...inputs) => {
+      return inputs.every(input => {
+        return input > 0;
+      });
+    };
+
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+
+      if (
+        !validInputs(distance, duration, cadence) ||
+          !positiveNumber(distance, duration, cadence)
+      )
+            return alert('Inputs have to be positive numbers!');
+
+        workout = new Running([lat, lng], distance, duration, cadence);
+
+        inputCadence.value =
+          inputDistance.value =
+          inputDuration.value =
+          inputElevation.value =
+            '';
+        form.classList.add('hidden');
+    }
+
+    if (type === 'cycling') {
+      const elevationGain = +inputElevation.value;
+
+      if (
+        !validInputs(distance, duration, elevationGain) ||
+        !positiveNumber(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers!');
+
+      workout = new Cycling([lat, lng], distance, duration, elevationGain);
+
+      inputCadence.value =
+        inputDistance.value =
+        inputDuration.value =
+        inputElevation.value =
+          '';
+      form.classList.add('hidden');
+    }
+
     L.marker([lat, lng])
       .addTo(map)
       .bindPopup(
@@ -69,19 +140,18 @@ form.addEventListener('submit', (e) => {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${type}-popup`,
         })
       )
       .setPopupContent('Workout')
         .openPopup();
     
-    inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = ''
-    form.classList.add('hidden')
+    workouts.push(workout)
+    console.log(workouts)
 })
 
 inputType.addEventListener('change', () => {
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
 })
-
 
